cecf4fb6746017e74c0329c06b26d16fd53ad837
yashverma2110 <yash.silver2110@gmail.com>
2020-12-17 18:59:43 +0530

    regex imporvements

--- src/utils/const.js
+++ src/utils/const.js
@@ -1,5 +1,5 @@
 export const emailRegx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-export const nameRegx = /^[A-Za-z]+$/;
+export const nameRegx = /^[a-z ,.'-]+$/i;
 export const boardTypes = [
   "To Do",
   "In development",
